36c17a359b7ed5ecdaa37b9ca90b57b874b9d5f6
“noellehou” <nonohouz@gmail.com>
2022-03-20 14:12:28 +0100

    commit

--- main/raytracing.cpp
+++ main/raytracing.cpp
@@ -21,8 +21,8 @@
 
 #include <cmath>
 #include <thread>
-
-#define OMP_NUM_THREADS = 16
+//#include<omp.h>
+//#define OMP_NUM_THREADS = 16
 
 Vector operator+(const Vector& a, const Vector &b) {
     return Vector(a[0]+b[0], a[1]+b[1], a[2]+b[2]);
@@ -108,7 +108,7 @@ Vector getColor(Ray &r, const Scene &s, int nbrebonds,bool show_lights=true) {
             } else {
 
         //Contribution de l'éclairage direct
-            /*
+            
             Ray ray_light(P + 0.01*N, (s.position_lumiere - P).getNormalized());
             Vector P_light, N_light;
             int sphere_id_light;
@@ -121,7 +121,7 @@ Vector getColor(Ray &r, const Scene &s, int nbrebonds,bool show_lights=true) {
             else{
                 intensite_pixel = s.objects[sphere_id]->albedo / M_PI * s.intensite_lumiere * std::max(0., dot((s.position_lumiere-P).getNormalized(),N)) / d_light2;
             } 
-             */
+             /*
             Vector axeOP = (P - s.lumiere->O).getNormalized();
             Vector dir_aleatoire = random_cos(axeOP);
             Vector point_aleatoire = dir_aleatoire * s.lumiere->R + s.lumiere->O; 
@@ -144,7 +144,7 @@ Vector getColor(Ray &r, const Scene &s, int nbrebonds,bool show_lights=true) {
                 // double proba = dot(axeOP, dir_aleatoire) / (M_PI * s.lumiere->R*s.lumiere->R);
                 // intensite_pixel = ( s.intensite_lumiere * std::max(0.,dot(N,wi)) * dot(Np, 0-wi) / dot(axeOP, dir_aleatoire) ) * s.objects[sphere_id]->albedo ;
             }
-        
+         
 
         //Ajout de la contribution de l'éclairage indirect
         
@@ -154,8 +154,9 @@ Vector getColor(Ray &r, const Scene &s, int nbrebonds,bool show_lights=true) {
 
             intensite_pixel += getColor(rayon_aleatoire, s, nbrebonds - 1) * s.objects[sphere_id]->albedo;
         
-
+*/
             }
+           
         }       
     }
     return intensite_pixel;
@@ -166,11 +167,9 @@ int main() {
     
 	int W = 100;
 	int H = 100;
-    const int nrays = 80;
+    const int nrays = 10;
 	double fov = 60*M_PI/180;
 
-    // TriangleMesh mesh = TriangleMesh();
-    // mesh.readOBJ("dog.obj");
     
 
     Sphere slum(Vector(15, 70, -30), 15, Vector(1.,1.,1.));
@@ -180,29 +179,29 @@ int main() {
     //Sphere s3(Vector(15,0,-75), 10, Vector(1,1,1),true);
     const char* nom_image = "test_mesh.png";
 
-    Sphere sol(Vector(0,-2000-20,0), 2000, Vector(0.4,0.4,1)); //sol
+    Sphere sol(Vector(0,-2000-20,0), 2000, Vector(1,1,1)); //sol
     Sphere plafond(Vector(0,2000+100,0), 2000, Vector(1,1,1)); //plafond
-    Sphere murgauche(Vector(-2000-50,0,0), 2000, Vector(0,0,1)); //mur gauche
-    Sphere murdroit(Vector(2000+50,0,0), 2000, Vector(0.2,0.8,1)); //mur droit
+    Sphere murgauche(Vector(-2000-50,0,0), 2000, Vector(1,1,1)); //mur gauche
+    Sphere murdroit(Vector(2000+50,0,0), 2000, Vector(1,1,1)); //mur droit
     Sphere murfond(Vector(0,0,-2000-100), 2000, Vector(1,1,1)); //mur fond
-    // TriangleMesh mesh_test(Vector(255,255,255), false,false);
-    //mesh_test.readOBJ("/Users/noelle/Documents/2 - CENTRALE LYON/MOS/MOS 2.2 - Infographie/informatiquegraphique/mesh/dog.obj");
-    Triangle tri(Vector(-10,-10,-20),Vector(10,-10,-20),Vector(0,10,-20),Vector(1,0,0));
+    TriangleMesh mesh_test(Vector(255,255,255), false,false);
+    mesh_test.readOBJ("/Users/noelle/Documents/2 - CENTRALE LYON/MOS/MOS 2.2 - Infographie/informatiquegraphique/mesh/dog.obj");
+    //Triangle tri(Vector(-10,-10,-20),Vector(10,-10,-20),Vector(0,10,-20),Vector(1,0,0));
 
     Scene s; 
     //s.addSphere(slum);
     //s.addSphere(s1);
     //s.addSphere(s2);
     //s.addSphere(s3);
-    s.addTriangle(tri);
-    //s.addMesh(mesh_test);
+    //s.addTriangle(tri);
+    s.addMesh(mesh_test);
     s.addSphere(sol);
     s.addSphere(plafond);
     s.addSphere(murgauche);
     s.addSphere(murdroit);
     s.addSphere(murfond);
     s.lumiere = &slum;
-    s.intensite_lumiere = 5000000000; 
+    s.intensite_lumiere = 1000000000; 
     Vector position_camera(0.,0.,50.);
     double focus_distance = 55;
     double aperture = 0.5;
@@ -210,7 +209,7 @@ int main() {
 
 	std::vector<unsigned char> image(W*H * 3, 0);
 
-#pragma omp parallel for num_threads(OMP_NUM_THREADS)
+#pragma omp parallel for 
 	for (int i = 0; i < H; i++) {
 		for (int j = 0; j < W; j++) {
 
@@ -242,7 +241,7 @@ int main() {
 		}
 	}
 
-	stbi_write_png("bille_eclairagedirect2_miroir.png", W, H, 3, &image[0], 0);
+	stbi_write_png("test_chien.png", W, H, 3, &image[0], 0);
 
 	return 0;
 }
